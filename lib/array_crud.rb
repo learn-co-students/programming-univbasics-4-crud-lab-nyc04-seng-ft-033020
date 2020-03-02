72b798a97b541b54f6763655cd187c692ef13e5b
jiangmichael1 <jiangm1@yahoo.com>
2020-03-01 19:12:09 -0500

    Done via vscode

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,41 +1,39 @@
 def create_an_empty_array
-  [ ]
+  array = []
 end
 
 def create_an_array
-  [ 1, 2, 3, 4]
+  array = [1,2,3,4]
 end
 
-def add_element_to_end_of_array(array, element)
-  array = ["earth","water","fire"]
-  array = array.push(element)
+def add_element_to_end_of_array(array,element)
+  array << element
 end
 
-def add_element_to_start_of_array(array, element)
-  array = ["earth","water","fire"]
-  array = array.unshift(element)
+def add_element_to_start_of_array(array,element)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array = array.shift
+  array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  array[index_number]
+def retrieve_element_from_index(array,index)
+  return array[index]
 end
 
 def retrieve_first_element_from_array(array)
-  array[0]
+  return array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  array[-1]
+  return array[-1]
 end
 
-def update_element_from_index(array, index_number, element)
-  array[index_number] = element
+def update_element_from_index(array,index,element)
+  array[index] = element
 end

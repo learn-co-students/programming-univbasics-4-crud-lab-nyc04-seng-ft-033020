711814d11e2a6a658b46443b5b325534e9ba713d
anna bateman <annacbateman@gmail.com>
2020-03-23 16:58:27 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,30 @@
-def create_an_empty_array
-  
+def create_an_empty_array 
+  []
 end
 
 def create_an_array
-  
+  covid19_supplies = ["toilet paper", "hand sanatizer", "groceries", "more toilet paper"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+  element = "arrays!"
+  array.push (element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  element = "wow"
+  array.unshift (element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+ 
 end
 
 def retrieve_element_from_index(array, index_number)
